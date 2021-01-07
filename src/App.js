65e232964e47c36d723ca3472ac9d51cc7990e46
matthewtong <tong.ma@northeastern.edu>
2021-01-06 19:11:42 -0500

    adding a 'view my work' button, not sure if it works yet

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import './App.css';
-import React from 'react';
+import React, {Component} from 'react';
 import AboutMePage from "./aboutMe/AboutMePage";
 
-function App() {
-  return (
-      <div>
-    <div className="App">
-      <header className="App-header">
+// function App() {
+//   return (
+//       <div>
+//     <div className="App">
+//       <header className="App-header">
+//         <div>
+//           Hi, I'm Matthew Tong
+//         </div>
+//         <div className="Sub-Header">
+//           <p>
+//             Welcome to my interactive resume
+//           </p>
+//         </div>
+//         <div>
+//           <button className="View-my-work">
+//               View my work
+//           </button>
+//         </div>
+//       </header>
+//     </div ref={this.content} id="content" class="section">
+//       <AboutMePage/>
+//       </div>
+//   );
+// }
+
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.content = React.createRef();
+        this.scrollToContent = this.scrollToContent.bind(this)
+    }
+
+    scrollToContent() {
+        this.content.current.scrollIntoView({ behavior: 'smooth'})
+    }
+
+  render() {
+    return (
         <div>
-          Hi, I'm Matthew Tong
+          <div className="App">
+            <header className="App-header">
+              <div>
+                Hi, I'm Matthew Tong
+              </div>
+              <div className="Sub-Header">
+                <p>
+                  Welcome to my interactive resume
+                </p>
+              </div>
+              <div>
+                <button className="View-my-work">
+                  View my work
+                </button>
+              </div>
+            </header>
+          </div>
+          <div ref={this.content} id="content">
+            <AboutMePage/>
+          </div>
         </div>
-        <div className="Sub-Header">
-          <p>
-            Welcome to my interactive resume
-          </p>
-        </div>
-      </header>
-    </div>
-      <AboutMePage/>
-      </div>
-  );
+    );
+  }
 }
 
 export default App;
